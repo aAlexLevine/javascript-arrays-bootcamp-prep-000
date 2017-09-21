36cd42f8f9bbc6090483e13cce9a24cdeea75a0b
Alex Levine <aalexlevine@gmail.com>
2017-09-21 15:28:32 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,5 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(aray, element) {
-  return addElementToBeginningOfArray()
-
-
-
+  return [element, array...]
 }
