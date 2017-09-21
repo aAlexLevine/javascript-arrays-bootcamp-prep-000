9c5560c88fe8273eb4ca474d3d9ff269a7cc96ad
Alex Levine <aalexlevine@gmail.com>
2017-09-21 15:13:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,7 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-/*function addElementToBeginningOfArray("element", "array") {
+function addElementToBeginningOfArray("element", "array") {
   addElementToBeginningOfArray.unshift(element)
   return addElementToBeginningOfArray()
 }
-*/
+
